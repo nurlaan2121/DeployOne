4b0eb026cff68f58175e44d3fc6bc13359690c6c
nurlan2121 <nurlanabijbillaev@gmail.com>
2024-06-04 19:25:08 +0600

    4

--- src/main/java/project/deployone/DeployOneApplication.java
+++ src/main/java/project/deployone/DeployOneApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeployOneApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeployOneApplication.class, args);
+
     }
 }
 
